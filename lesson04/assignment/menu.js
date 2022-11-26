aea6226e09c0aa7d1bf3d99fec74081df4225a00
Laflad <laflad04@gmail.com>
2022-11-25 16:59:12 -0800

    saving file but no change to assignment

--- lesson04/assignment/menu.js
+++ lesson04/assignment/menu.js
@@ -1,17 +1,17 @@
 // I tried to create an innerHTML for the nav so that I only have to make updates in a single location, but I was struggling so I'll save the file here for a future assignment and focus on the actual assignment for now. :)
 
-const topMenu = document.querySelector('.topnav')
+// const topMenu = document.querySelector('.topnav')
 
-    let mainNav = document.createElement('div')
-    mainNav.innerHTML = `
-        <div class="topnav">
-            <a class="active" href="index.html">Home</a>
-            <a href="404.html">Logo (coming soon)</a>
-            <a href="colors.html">Colors</a>
-            <a href="404.html">Voice (coming soon)</a>
-            <a href="404.html">Images (coming soon)</a>
-        </div>
-        `
-    topMenu.append(mainNav)
+//     let mainNav = document.createElement('div')
+//     mainNav.innerHTML = `
+//         <div class="topnav">
+//             <a class="active" href="index.html">Home</a>
+//             <a href="404.html">Logo (coming soon)</a>
+//             <a href="colors.html">Colors</a>
+//             <a href="404.html">Voice (coming soon)</a>
+//             <a href="404.html">Images (coming soon)</a>
+//         </div>
+//         `
+//     topMenu.append(mainNav)
 
-console.log(topMenu)
+// console.log(topMenu)
